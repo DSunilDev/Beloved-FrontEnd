1036d3863f65dae78053b932769b31610204e223
DSunilDev <sunilkumardeveops@gmail.com>
2023-05-19 16:15:57 +0530

    Added Js Folder

---
+++ JavaScript/app.js
@@ -0,0 +1,13 @@
+console.dir(document);
+document.children[0].children[1].children[1].children[0].href="https:\\www.google.com";
+
+
+
+let k=document.createElement('a')
+k.href='https:\\www.dsunil.in'
+k.textContent="Sunil's website"
+
+let y=document.getElementById('#p')
+y.textContent="Hello Everyone";
+y.append(k);
+
